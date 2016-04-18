3534173ddb5eb47d9fac337a837fdd9fb9e8e9b7
masterUNG <phrombutr@gmail.com>
2016-04-18 11:47:03 +0700

    mySetCalendar

--- app/src/main/java/com/example/oak/test170416/SetCalendarActivity.java
+++ app/src/main/java/com/example/oak/test170416/SetCalendarActivity.java
@@ -2,8 +2,11 @@ package com.example.oak.test170416;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.CalendarView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class SetCalendarActivity extends AppCompatActivity {
 
@@ -27,13 +30,35 @@ public class SetCalendarActivity extends AppCompatActivity {
                                             int day) {
 
                 Toast.makeText(SetCalendarActivity.this,
-                        day + "/" + month + "/" + year,
+                        day + "/" + (month + 1) + "/" + year,
                         Toast.LENGTH_SHORT).show();
+
+                setMyAlarm(day, month, year);
 
 
             }   // onSelected
         });
 
     } // Main Method
+
+    private void setMyAlarm(int day, int month, int year) {
+
+        String tag = "18April";
+
+        Calendar calendar = Calendar.getInstance();
+
+        int intMinus = calendar.get(Calendar.MINUTE);
+        intMinus = intMinus + 2;
+
+        Calendar mySetCalendar1 = (Calendar) calendar.clone();
+        mySetCalendar1.set(Calendar.DAY_OF_MONTH, day);
+        mySetCalendar1.set(Calendar.MONTH, month);
+        mySetCalendar1.set(Calendar.YEAR, year);
+        mySetCalendar1.set(Calendar.MINUTE, intMinus);
+
+        Log.d(tag, "mySetCalendar1 ==> " + mySetCalendar1.toString());
+
+
+    }   // setMyAlarm
 
 }   // Main Class
