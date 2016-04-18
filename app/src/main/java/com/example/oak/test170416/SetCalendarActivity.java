ec2f4c2cfacf77375aefbf16843869ede6203905
masterUNG <phrombutr@gmail.com>
2016-04-18 14:44:33 +0700

    Finish

--- app/src/main/java/com/example/oak/test170416/SetCalendarActivity.java
+++ app/src/main/java/com/example/oak/test170416/SetCalendarActivity.java
@@ -1,9 +1,15 @@
 package com.example.oak.test170416;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.CalendarView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -12,6 +18,8 @@ public class SetCalendarActivity extends AppCompatActivity {
 
     //Explicit
     private CalendarView calendarView;
+    private TimePickerDialog timePickerDialog;
+    private int dayAnInt, monthAnInt, yearAnInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,21 +52,55 @@ public class SetCalendarActivity extends AppCompatActivity {
     private void setMyAlarm(int day, int month, int year) {
 
         String tag = "18April";
+        Calendar currentCalendar = Calendar.getInstance();
 
-        Calendar calendar = Calendar.getInstance();
+        dayAnInt = day;
+        monthAnInt = month;
+        yearAnInt = year;
 
-        int intMinus = calendar.get(Calendar.MINUTE);
-        intMinus = intMinus + 2;
-
-        Calendar mySetCalendar1 = (Calendar) calendar.clone();
-        mySetCalendar1.set(Calendar.DAY_OF_MONTH, day);
-        mySetCalendar1.set(Calendar.MONTH, month);
-        mySetCalendar1.set(Calendar.YEAR, year);
-        mySetCalendar1.set(Calendar.MINUTE, intMinus);
-
-        Log.d(tag, "mySetCalendar1 ==> " + mySetCalendar1.toString());
-
+        timePickerDialog = new TimePickerDialog(SetCalendarActivity.this,
+                onTimeSetListener,
+                currentCalendar.get(Calendar.HOUR_OF_DAY),
+                currentCalendar.get(Calendar.MINUTE),
+                false);
+        timePickerDialog.setTitle("โปรดเลือกเวลา");
+        timePickerDialog.show();
 
     }   // setMyAlarm
+
+    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hourOfDay, int minus) {
+
+            Calendar calendar = Calendar.getInstance();
+            Calendar cloneCalendar1 = (Calendar) calendar.clone();
+
+            cloneCalendar1.set(Calendar.DAY_OF_MONTH, dayAnInt);
+            cloneCalendar1.set(Calendar.MONTH, monthAnInt);
+            cloneCalendar1.set(Calendar.YEAR, yearAnInt);
+            cloneCalendar1.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            cloneCalendar1.set(Calendar.MINUTE, minus);
+            cloneCalendar1.set(Calendar.SECOND, 0);
+
+            Log.d("18April", "cloneCalendar1 ==> " + cloneCalendar1.getTime());
+
+            mySetToAlarm(cloneCalendar1);
+
+        }   // onTimeSet
+    };
+
+
+
+    private void mySetToAlarm(Calendar mySetCalendar1) {
+
+        final int _id = (int) System.currentTimeMillis();
+
+        Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), _id, intent, 0);
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, mySetCalendar1.getTimeInMillis(), pendingIntent);
+
+
+    }   // mySetToAlarm
 
 }   // Main Class
